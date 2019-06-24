590fd413d0183d4e6a66d6b77d72b25bb2f36c1f
nikolenko <qoo2p5@gmail.com>
2019-06-24 23:21:50 +0300

    Fix code issues

--- tree.h
+++ tree.h
@@ -21,6 +21,9 @@ class Set {
 
         explicit Node(const ValueType& value) : value(value) {}
 
+        Node(const ValueType& value, Node* left, Node* right, Node* parent, size_t level) :
+                value(value), left(left), right(right), parent(parent), level(level) {}
+
         const Node* go_left() const {
             const Node* result = this;
             while (result->left->level > 0) {
@@ -38,8 +41,8 @@ class Set {
         }
 
         const Node* get_next() const {
-            if (this->right->level > 0) {
-                return this->right->go_left();
+            if (right->level > 0) {
+                return right->go_left();
             }
             const Node* cur = this;
             while (cur->level > 0) {
@@ -52,8 +55,8 @@ class Set {
         }
 
         const Node* get_previous() const {
-            if (this->left->level > 0) {
-                return this->left->go_right();
+            if (left->level > 0) {
+                return left->go_right();
             }
             const Node* cur = this;
             while (cur->level > 0) {
@@ -72,49 +75,49 @@ class Set {
         iterator() : node(nullptr), is_end(false) {}
 
         const ValueType& operator*() const {
-            return this->node->value;
+            return node->value;
         }
 
         const ValueType* operator->() const {
-            return &(this->node->value);
+            return &(node->value);
         }
 
         bool operator==(const iterator& other) const {
-            return this->node == other.node && this->is_end == other.is_end;
+            return node == other.node && is_end == other.is_end;
         }
 
         bool operator!=(const iterator& other) const {
-            return this->node != other.node || this->is_end != other.is_end;
+            return node != other.node || is_end != other.is_end;
         }
 
         iterator operator++() {
-            const Node* next = this->node->get_next();
+            const Node* next = node->get_next();
             if (next->level == 0) {
-                this->is_end = true;
+                is_end = true;
             } else {
-                this->node = next;
+                node = next;
             }
             return *this;
         }
 
         const iterator operator++(int) {
             iterator tmp = *this;
-            this->operator++();
+            operator++();
             return tmp;
         }
 
         iterator operator--() {
-            if (this->is_end) {
-                this->is_end = false;
+            if (is_end) {
+                is_end = false;
             } else {
-                this->node = this->node->get_previous();
+                node = node->get_previous();
             }
             return *this;
         }
 
         const iterator operator--(int) {
             iterator tmp = *this;
-            this->operator--();
+            operator--();
             return tmp;
         }
 
@@ -134,67 +137,66 @@ class Set {
     };
 
     Set() {
-        this->initialize();
+        initialize();
     }
 
     template<class InputIterator>
     Set(InputIterator first, InputIterator last) {
-        this->initialize();
+        initialize();
         while (first != last) {
-            this->insert(*first++);
+            insert(*first++);
         }
     }
 
     Set(std::initializer_list<ValueType> init) {
-        this->initialize();
+        initialize();
         for (const ValueType& value : init) {
-            this->insert(value);
+            insert(value);
         }
     }
 
     Set(const Set<ValueType>& other) {
-        this->initialize();
+        initialize();
         for (const ValueType& value : other) {
-            this->insert(value);
+            insert(value);
         }
     }
 
     Set& operator=(const Set<ValueType>& other) {
         if (this != &other) {
-            this->clear();
+            clear();
             for (const ValueType& value : other) {
-                this->insert(value);
+                insert(value);
             }
         }
         return *this;
     }
 
     ~Set() {
-        this->clear();
-        delete this->bottom;
+        clear();
+        delete bottom;
     }
 
     size_t size() const {
-        return this->node_count;
+        return node_count;
     }
 
     bool empty() const {
-        return this->node_count == 0;
+        return node_count == 0;
     }
 
     void insert(const ValueType& value) {
         bool is_inserted;
-        this->root = internal_insert(this->root, is_inserted, value);
+        root = internal_insert(root, &is_inserted, value);
         if (is_inserted) {
             node_count++;
         }
     }
 
     void erase(const ValueType& value) {
-        to_erase = nullptr;
         bool is_erased;
-        this->root = internal_erase(this->root, is_erased, value);
-        this->root->parent = this->bottom;
+        root = internal_erase(root, &is_erased, value);
+        root->parent = bottom;
         if (is_erased) {
             node_count--;
         }
@@ -202,17 +204,13 @@ class Set {
 
     iterator lower_bound(const ValueType& value) const {
         Node* result = internal_find(value);
-        if (result == this->bottom) {
-            return this->end();
-        } else {
-            return iterator(result);
-        }
+        return (result == bottom) ? end() : iterator(result);
     }
 
     iterator find(const ValueType& value) const {
         Node* v = internal_find(value);
-        if (v == this->bottom || value < v->value) {
-            return this->end();
+        if (v == bottom || value < v->value) {
+            return end();
         } else {
             return iterator(v);
         }
@@ -227,9 +225,9 @@ class Set {
     }
 
     void clear() {
-        this->recursive_delete(root);
-        this->root = bottom;
-        this->node_count = 0;
+        recursive_delete(root);
+        root = bottom;
+        node_count = 0;
     }
 
  private:
@@ -238,30 +236,25 @@ class Set {
     Node* root;
 
     void initialize() {
-        this->node_count = 0;
-        this->bottom = new Node();
-        this->bottom->level = 0;
-        this->bottom->left = this->bottom;
-        this->bottom->right = this->bottom;
-        this->bottom->parent = this->bottom;
-        this->root = this->bottom;
+        node_count = 0;
+        bottom = new Node();
+        bottom->level = 0;
+        bottom->left = bottom;
+        bottom->right = bottom;
+        bottom->parent = bottom;
+        root = bottom;
     }
 
     Node* make_node(const ValueType& value) {
-        Node* result = new Node(value);
-        result->level = 1;
-        result->left = this->bottom;
-        result->right = this->bottom;
-        result->parent = this->bottom;
-        return result;
+        return new Node(value, bottom, bottom, bottom, 1);
     }
 
     void recursive_delete(Node* node) {
-        if (node == this->bottom) {
+        if (node == bottom) {
             return;
         }
-        this->recursive_delete(node->left);
-        this->recursive_delete(node->right);
+        recursive_delete(node->left);
+        recursive_delete(node->right);
         delete node;
     }
 
@@ -269,7 +262,7 @@ class Set {
         Node* parent = v->parent;
         Node* new_v = v->right;
         v->right = new_v->left;
-        if (v->right != this->bottom) {
+        if (v->right != bottom) {
             v->right->parent = v;
         }
         new_v->left = v;
@@ -287,7 +280,7 @@ class Set {
         Node* parent = v->parent;
         Node* new_v = v->left;
         v->left = new_v->right;
-        if (v->left != this->bottom) {
+        if (v->left != bottom) {
             v->left->parent = v;
         }
         new_v->right = v;
@@ -302,30 +295,30 @@ class Set {
     }
 
     Node* skew(Node* node) {
-        if (node == this->bottom) {
+        if (node == bottom) {
             return node;
         }
         if (node->level == node->left->level) {
-            return this->rotate_right(node);
+            return rotate_right(node);
         }
         return node;
     }
 
     Node* split(Node* node) {
-        if (node == this->bottom) {
+        if (node == bottom) {
             return node;
         }
         if (node->level == node->right->level && node->level == node->right->right->level) {
             node->right->level++;
-            return this->rotate_left(node);
+            return rotate_left(node);
         }
         return node;
     }
 
     Node* internal_find(const ValueType& value) const {
-        Node* cur = this->root;
-        Node* last = this->bottom;
-        while (cur != this->bottom) {
+        Node* cur = root;
+        Node* last = bottom;
+        while (cur != bottom) {
             if (value < cur->value) {
                 last = cur;
                 cur = cur->left;
@@ -339,62 +332,64 @@ class Set {
         return last;
     }
 
-    Node* internal_insert(Node* node, bool& is_inserted, const ValueType& value) {
-        if (node == this->bottom) {
-            is_inserted = true;
-            return this->make_node(value);
+    Node* internal_insert(Node* node, bool* is_inserted, const ValueType& value) {
+        if (node == bottom) {
+            *is_inserted = true;
+            return make_node(value);
         }
 
         if (value < node->value) {
-            node->left = this->internal_insert(node->left, is_inserted, value);
+            node->left = internal_insert(node->left, is_inserted, value);
             node->left->parent = node;
         } else if (node->value < value) {
-            node->right = this->internal_insert(node->right, is_inserted, value);
+            node->right = internal_insert(node->right, is_inserted, value);
             node->right->parent = node;
         } else {
-            is_inserted = false;
+            *is_inserted = false;
         }
 
-        if (is_inserted) {
+        if (*is_inserted) {
             node = skew(node);
             node = split(node);
         }
         return node;
     }
 
-    Node* to_erase;
+    Node* internal_erase(Node* node, bool* is_erased, const ValueType& value) {
+        Node* to_erase = nullptr;
+        Node* last = nullptr;
+        return internal_erase(node, is_erased, value, &to_erase, &last);
+    }
 
-    Node* internal_erase(Node* node, bool& is_erased, const ValueType& value) {
-        static Node* last;
-
-        is_erased = false;
-        if (node == this->bottom) {
+    Node* internal_erase(Node* node, bool* is_erased, const ValueType& value, Node** to_erase, Node** last) {
+        *is_erased = false;
+        if (node == bottom) {
             return node;
         }
-        last = node;
+        *last = node;
 
         if (value < node->value) {
-            node->left = internal_erase(node->left, is_erased, value);
-            if (node->left != this->bottom) {
+            node->left = internal_erase(node->left, is_erased, value, to_erase, last);
+            if (node->left != bottom) {
                 node->left->parent = node;
             }
         } else {
-            to_erase = node;
-            node->right = internal_erase(node->right, is_erased, value);
-            if (node->right != this->bottom) {
+            *to_erase = node;
+            node->right = internal_erase(node->right, is_erased, value, to_erase, last);
+            if (node->right != bottom) {
                 node->right->parent = node;
             }
         }
 
-        if (node == last && to_erase && !(to_erase->value < value) && !(value < to_erase->value)) {
-            is_erased = true;
-            to_erase->value = last->value;
-            if (last == to_erase) {
-                node = last->left;
+        if (node == *last && *to_erase && !((*to_erase)->value < value) && !(value < (*to_erase)->value)) {
+            *is_erased = true;
+            (*to_erase)->value = (*last)->value;
+            if (*last == *to_erase) {
+                node = (*last)->left;
             } else {
-                node = last->right;
+                node = (*last)->right;
             }
-            delete last;
+            delete *last;
         }
 
         if (node->left->level + 1 < node->level || node->right->level + 1 < node->level) {
@@ -402,14 +397,13 @@ class Set {
             if (node->right->level > node->level) {
                 node->right->level--;
             }
-            node = this->skew(node);
-            this->skew(node->right);
-            this->skew(node->right->right);
-            node = this->split(node);
-            this->split(node->right);
+            node = skew(node);
+            skew(node->right);
+            skew(node->right->right);
+            node = split(node);
+            split(node->right);
         }
 
         return node;
     }
 };
-
